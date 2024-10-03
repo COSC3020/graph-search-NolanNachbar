fce2f79c46f5ead49ea1de022b493322be40dbc6
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-10-03 17:23:09 -0600

    Update code.js

--- code.js
+++ code.js
@@ -23,3 +23,57 @@ function depthFirstSearch(graph, startNode, targetNode) { // note that graph wil
     depthFirstHelp(startNode);
     return path;
 }
+
+const graph = [
+    [3, 4, 2, 7],
+    [0],
+    [1, 4],
+    [2, 3, 6],
+    [1, 5],
+    [6],
+    [],
+    [8],
+    [],
+    []
+];
+
+const graph2 = [
+    [1, 2],
+    [3],
+    [3, 4],
+    [],
+    [5, 6],
+    [],
+    [7],
+    [],
+    [],
+    []
+];
+
+const graph3 = [
+    [1],
+    [2, 3],
+    [4],
+    [7],
+    [5],
+    [],
+    [8],
+    []
+];
+
+const startNode = 0;
+var targetNode = 7;
+
+function testSearch(theAdjList){
+  const findPath = depthFirstSearch(graph, startNode, targetNode);
+  
+  if (findPath.length === 0 ){console.log("Zero");}
+  else if (findPath[findPath.length - 1] == targetNode)
+    console.log(`Test successful, Path from node ${startNode} to node ${targetNode}:`, findPath)
+  else if (findPath[findPath.length - 1] !== targetNode)
+    console.error(`Fail, no path found`);
+}
+
+testSearch(graph)
+testSearch(graph2)
+testSearch(graph3)
