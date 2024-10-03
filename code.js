a4536a9337f79a1b698d99f90ac0e8822a552f21
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-10-03 16:52:22 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,40 +1,25 @@
 function depthFirstSearch(graph, startNode, targetNode) { // note that graph will be an adjacency list
     let visited = [] 
+    let path = []
     
     function depthFirstHelp(node){
         if (visited.includes(node)) return false;
-        if (node === targetNode) return true;
+        if (node === targetNode) {
+          path.push(node);
+          return true;
+        }
         
         visited.push(node);
+        path.push(node);
         for(let i = 0; i < graph[node].length; i++){
-            if(!visited.includes(grap[node][i]))
-            depthFirstHelp(graph[node][i]);
+            if(depthFirstHelp(graph[node][i]))
+              return true;
         }
-        console.log(visited)
-        visited.pop(); // pop it because the target wasn;t found on it's path
-        return visited;
+        
+        path.pop(); // pop it because the target wasn't found on it's path
+        return false;
     }
     
-    let returnPath = depthFirstHelp(graph, startNode, targetNode);
-    return returnPath;
+    depthFirstHelp(startNode);
+    return path;
 }
-
-// Example graph
-const graph3 = [
-    [2, 3, 6],
-    [3],
-    [0],
-    [0, 1, 7],
-    [],
-    [],
-    [0],
-    [8, 3],
-    [7]
-];
-
-// Running the DFS
-const startNode = 0;
-const targetNode = 7;
-const path = depthFirstSearch(graph3, startNode, targetNode);
-
-console.log(`Path from node ${startNode} to node ${targetNode}:`, path);
