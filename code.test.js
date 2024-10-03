1214db8f30d5a2031959a6057d10d8dae0c47819
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-10-03 17:27:04 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -3,6 +3,45 @@ const jsc = require('jsverify');
 
 eval(fs.readFileSync('code.js')+'');
 
+
+const graph = [
+    [3, 4, 2, 7],
+    [0],
+    [1, 4],
+    [2, 3, 6],
+    [1, 5],
+    [6],
+    [],
+    [8],
+    [],
+    []
+];
+
+// Test cases
+const graph2 = [
+    [1, 2],
+    [3],
+    [3, 4],
+    [],
+    [5, 6],
+    [],
+    [7],
+    [],
+    [],
+    []
+];
+
+const graph3 = [
+    [1],
+    [2, 3],
+    [4],
+    [7],
+    [5],
+    [],
+    [8],
+    []
+];
+
 // Running the DFS
 const startNode = 0;
 var targetNode = 7;
