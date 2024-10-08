fce46d295beb4129a858154278d509d7f29c4127
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-10-08 11:35:51 -0600

    Update code.test.js
    Added functionality to actually compare the target paths

--- code.test.js
+++ code.test.js
@@ -2,11 +2,9 @@ const fs = require('fs');
 const jsc = require('jsverify');
 eval(fs.readFileSync('code.js')+'');
 
-// Running the DFS
+// Running tests
 const startNode = 0;
-var targetNode = 7;
-
-
+const targetNode = 7;
 const graph = [
     [3, 4, 2, 7],
     [0],
@@ -45,53 +43,47 @@ const graph3 = [
 ];
 
 
-
-
-const testSearch1 = (depthFirstSearch(graph, startNode, targetNode) == [ 0, 7 ] )
-jsc.assert(testSearch1);
-if (depthFirstSearch(graph, startNode, targetNode) == [ 0, 7 ] ) {
-  console.log('DFS Test successful');
+function arraysEqual(arr1, arr2) {
+    return JSON.stringify(arr1) === JSON.stringify(arr2);
+}
+const testSearch1 = depthFirstSearch(graph, startNode, targetNode);
+if (arraysEqual(testSearch1, [0, 7])) {
+  console.log('DFS Test 1 successful');
 } else {
-  console.error(`DFS Fail, ${depthFirstSearch(graph, startNode, targetNode)} != [ 0, 7 ]$`); 
+  console.error(`DFS Test 1 Fail, ${testSearch1} != [ 0, 7 ]`); 
 }
 
-const testSearch2 = (depthFirstSearch(graph2, startNode, targetNode) == [ 0, 2, 4, 6, 7 ]) 
-jsc.assert(testSearch2);
-if (depthFirstSearch(graph2, startNode, targetNode) == [ 0, 2, 4, 6, 7 ]) {
-  console.log('DFS Test successful');
-}else{ 
-  console.error((`DFS Fail, ${depthFirstSearch(graph2, startNode, targetNode)} != [ 0, 2, 4, 6, 7 ]$`));
-}
-
-const testSearch3 = (depthFirstSearch(graph3, startNode, targetNode) == [ 0, 1, 3, 7 ] )
-jsc.assert(testSearch3);
-if (depthFirstSearch(graph3, startNode, targetNode) == [ 0, 1, 3, 7 ] ) {
-  console.log('DFS Test successful');
+const testSearch2 = depthFirstSearch(graph2, startNode, targetNode);
+if (arraysEqual(testSearch2, [0, 2, 4, 6, 7])) {
+  console.log('DFS Test 2 successful');
 } else {
-  console.error(`DFS Fail, ${depthFirstSearch(graph3, startNode, targetNode)} !=  [ 0, 1, 3, 7 ] $`); 
+  console.error(`DFS Test 2 Fail, ${testSearch2} != [ 0, 2, 4, 6, 7 ]`); 
 }
 
-
-const testSearch4 = (breadthFirstSearch(graph, startNode, targetNode) == [ 0, 7 ] )
-jsc.assert(testSearch4);
-if (breadthFirstSearch(graph, startNode, targetNode) == [ 0, 7 ] ) {
-  console.log('BFS Test successful');
+const testSearch3 = depthFirstSearch(graph3, startNode, targetNode);
+if (arraysEqual(testSearch3, [0, 1, 3, 7])) {
+  console.log('DFS Test 3 successful');
 } else {
-  console.error(`BFS Fail, ${breadthFirstSearch(graph, startNode, targetNode)} != [ 0, 7 ]$`); 
+  console.error(`DFS Test 3 Fail, ${testSearch3} != [ 0, 1, 3, 7 ]`); 
 }
 
-const testSearch5 = (breadthFirstSearch(graph2, startNode, targetNode) == [ 0, 2, 4, 6, 7 ])
-jsc.assert(testSearch5);
-if (breadthFirstSearch(graph2, startNode, targetNode) == [ 0, 2, 4, 6, 7 ]) {
-  console.log('BFS Test successful');
-}else{ 
-  console.error((`BFS Fail, ${breadthFirstSearch(graph2, startNode, targetNode)} != [ 0, 2, 4, 6, 7 ]$`));
-}
-
-const testSearch6 = (breadthFirstSearch(graph3, startNode, targetNode) == [ 0, 1, 3, 7 ] )
-jsc.assert(testSearch6);
-if (breadthFirstSearch(graph3, startNode, targetNode) == [ 0, 1, 3, 7 ] ) {
-  console.log('BFS Test successful');
+const testSearch4 = breadthFirstSearch(graph, startNode, targetNode);
+if (arraysEqual(testSearch4, [0, 7])) {
+  console.log('BFS Test 4 successful');
 } else {
-  console.error(`BFS Fail, ${breadthFirstSearch(graph3, startNode, targetNode)} !=  [ 0, 1, 3, 7 ] $`); 
+  console.error(`BFS Test 4 Fail, ${testSearch4} != [ 0, 7 ]`); 
+}
+
+const testSearch5 = breadthFirstSearch(graph2, startNode, targetNode);
+if (arraysEqual(testSearch5, [0, 2, 4, 6, 7])) {
+  console.log('BFS Test 5 successful');
+} else {
+  console.error(`BFS Test 5 Fail, ${testSearch5} != [ 0, 2, 4, 6, 7 ]`); 
+}
+
+const testSearch6 = breadthFirstSearch(graph3, startNode, targetNode);
+if (arraysEqual(testSearch6, [0, 1, 3, 7])) {
+  console.log('BFS Test 6 successful');
+} else {
+  console.error(`BFS Test 6 Fail, ${testSearch6} != [ 0, 1, 3, 7 ]`); 
 }
