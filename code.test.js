bef83d8a439b008272de5764e2ac9991022be084
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-10-08 10:16:44 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,7 +1,8 @@
-const fs = require('fs');
-const jsc = require('jsverify');
 
-eval(fs.readFileSync('code.js')+'');
+// Running the DFS
+const startNode = 0;
+var targetNode = 7;
+
 
 const graph = [
     [3, 4, 2, 7],
@@ -40,23 +41,55 @@ const graph3 = [
     []
 ];
 
-// Running the DFS
-const startNode = 0;
-var targetNode = 7;
+
+const fs = require('fs');
+const jsc = require('jsverify');
+
+const testSearch1 = (depthFirstSearch(graph, startNode, targetNode) == [ 0, 7 ] )
+jsc.assert(testSearch1);
+if (depthFirstSearch(graph, startNode, targetNode) == [ 0, 7 ] ) {
+  console.log('DFS Test successful');
+} else {
+  console.error(`DFS Fail, ${depthFirstSearch(graph, startNode, targetNode)} != [ 0, 7 ]$`); 
+}
+
+const testSearch2 = (depthFirstSearch(graph2, startNode, targetNode) == [ 0, 2, 4, 6, 7 ]) 
+jsc.assert(testSearch2);
+if (depthFirstSearch(graph2, startNode, targetNode) == [ 0, 2, 4, 6, 7 ]) {
+  console.log('DFS Test successful');
+}else{ 
+  console.error((`DFS Fail, ${depthFirstSearch(graph2, startNode, targetNode)} != [ 0, 2, 4, 6, 7 ]$`));
+}
+
+const testSearch3 = (depthFirstSearch(graph3, startNode, targetNode) == [ 0, 1, 3, 7 ] )
+jsc.assert(testSearch3);
+if (depthFirstSearch(graph3, startNode, targetNode) == [ 0, 1, 3, 7 ] ) {
+  console.log('DFS Test successful');
+} else {
+  console.error(`DFS Fail, ${depthFirstSearch(graph3, startNode, targetNode)} !=  [ 0, 1, 3, 7 ] $`); 
+}
 
 
-test('Test depthFirstSearch function', () => {
- expect(depthFirstSearch(graph, startNode, targetNode)).toEqual([ 0, 7 ]); 
- expect(depthFirstSearch(graph2, startNode, targetNode)).toEqual([ 0, 2, 4, 6, 7 ]); 
- expect(depthFirstSearch(graph3, startNode, targetNode)).toEqual( [ 0, 1, 3, 7 ]); 
-});
+const testSearch4 = (breadthFirstSearch(graph, startNode, targetNode) == [ 0, 7 ] )
+jsc.assert(testSearch4);
+if (breadthFirstSearch(graph, startNode, targetNode) == [ 0, 7 ] ) {
+  console.log('BFS Test successful');
+} else {
+  console.error(`BFS Fail, ${breadthFirstSearch(graph, startNode, targetNode)} != [ 0, 7 ]$`); 
+}
 
-test('Test breadthFirstSearch function', () => {
- expect(breadthFirstSearch(graph, startNode, targetNode)).toEqual([ 0, 7 ]); 
- expect(breadthFirstSearch(graph2, startNode, targetNode)).toEqual([ 0, 2, 4, 6, 7 ]); 
- expect(breadthFirstSearch(graph3, startNode, targetNode)).toEqual( [ 0, 1, 3, 7 ]); 
-});
+const testSearch5 = (breadthFirstSearch(graph2, startNode, targetNode) == [ 0, 2, 4, 6, 7 ])
+jsc.assert(testSearch5);
+if (breadthFirstSearch(graph2, startNode, targetNode) == [ 0, 2, 4, 6, 7 ]) {
+  console.log('BFS Test successful');
+}else{ 
+  console.error((`BFS Fail, ${breadthFirstSearch(graph2, startNode, targetNode)} != [ 0, 2, 4, 6, 7 ]$`));
+}
 
-BFStestSearch(graph)
-BFStestSearch(graph2)
-BFStestSearch(graph3)
+const testSearch6 = (breadthFirstSearch(graph3, startNode, targetNode) == [ 0, 1, 3, 7 ] )
+jsc.assert(testSearch6);
+if (breadthFirstSearch(graph3, startNode, targetNode) == [ 0, 1, 3, 7 ] ) {
+  console.log('BFS Test successful');
+} else {
+  console.error(`BFS Fail, ${breadthFirstSearch(graph3, startNode, targetNode)} !=  [ 0, 1, 3, 7 ] $`); 
+}
